e499ecf593894182517b2acdd5388985dff718f0
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-06 12:49:39 +0800

    Fix syntax errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,9 +26,9 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    if user_id = session[:user_id]
+    if (user_id = session[:user_id])
       @current_user ||= User.find_by_id(user_id)
-    elsif user_id = cookies.signed[:user_id]
+    elsif (user_id = cookies.signed[:user_id])
       user = User.find_by_id(user_id)
       remember_token = cookies[:remember_token]
       if user && remember_token && 
