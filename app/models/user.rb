b183f3e6d27977b5de5b2a2aaed66a879f37aaf9
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-06 09:30:17 +0800

    Add remember token to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_create :remember
   has_secure_password
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest token
+  end
+
+  def remember
+    remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 end
--- db/migrate/20160906010057_add_remember_digest_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddRememberDigestToUser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :remember_digest, :string
-  end
-end
