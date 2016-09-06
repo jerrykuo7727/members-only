8c100f63d7e01ea7c1ef1f7d9915eefbf77f108f
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-06 11:20:08 +0800

    Add actions to new and create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user?, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def logged_in_user?
+    logged_in?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160906015229) do
+ActiveRecord::Schema.define(version: 20160906030031) do
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
