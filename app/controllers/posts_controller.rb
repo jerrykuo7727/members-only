8b6118255dbb8720210a798affc47d859fd613c1
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-06 13:15:40 +0800

    Add some UI features

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
   before_action :logged_in_user?, only: [:new, :create]
 
   def new
+    @username = current_user.name
   end
 
   def create
@@ -19,6 +20,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    @username = current_user.name
   end
 
   private
