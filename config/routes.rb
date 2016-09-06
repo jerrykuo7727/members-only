52ed96d1eb198f79a50eb3004dd150234c2c5953
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-06 11:45:21 +0800

    Change root path to posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#home' 
+  root to: 'posts#index' 
   get    'signin',  to: 'sessions#new'
   post   'signin',  to: 'sessions#create'
   delete 'signout', to: 'sessions#delete'
