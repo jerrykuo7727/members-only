b9ede658bb448a8fce7242320dc61f06069fc0d0
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-06 12:29:53 +0800

    Build users#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(name:     params[:user][:name],
+                     email:    params[:user][:email],
+                     password: params[:user][:password],
+                     password_confirmation: params[:user][:password_confirmation])
+    if @user.valid?
+      @user.save
+      log_in(@user)
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 end
