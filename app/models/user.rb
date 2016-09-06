d46f63363610e7b037fcc2482005816aabd17bd1
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-06 12:13:13 +0800

    Add validation to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
+  before_save   :downcase_email
   before_create :remember
+  validates :name, presence:true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
+  vadlidates :password, presence:true, length { minimum 6 }
   has_many :posts
 
   def remember
@@ -12,5 +19,11 @@ class User < ApplicationRecord
 
   def forget
     self.remember_digest = nil
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
   end
 end
