aa72c7217a750a29bde0539e43239fe7a3077bdb
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-06 10:26:19 +0800

    Build curret_user helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def login(user)
+  def log_in(user)
     sessions[:user_id] = user.id
   end
 
@@ -17,5 +17,21 @@ class ApplicationController < ActionController::Base
     cookies.delete(:remember_token)
   end
 
+  def current_user
+    if user_id = sessions[:user_id]
+      @current_user ||= User.find_by_id(user_id)
+    elsif user_id = cookies.signed[:user_id]
+      user = User.find_by_id(user_id)
+      remember_token = cookies[:remember_token]
+      if user && remember_token &&
+         Digest::SHA1.hexdigest(remember_token) == user.remember_digest
+        log_in user
+        @current_user = user
+      end
+    end
+  end
 
+  def current_user=(user)
+    @current_user = user
+  end
 end
