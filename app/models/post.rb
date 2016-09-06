7e00227001b9b5178782c746a0452f686ed2c890
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-09-06 22:26:14 +0800

    Add CSS to pagination

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :text,  presence: true, length: { maximum: 10000 }
   belongs_to :user
+  self.per_page = 5
 end
